623af96f0495e3514dbe475461c5e821b51f70f5
Adam Habil <ahabil@outlook.com>
2020-11-05 08:16:31 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteTakingApp
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;            //Object creation
+        //
+        //Required for Designer support
+        //
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //
+        //Changes to the data table
+        //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Messages", typeof(String));
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Title"].Width = 152;
+            dataGridView1.Columns["Messages"].Width = 304;
+        }
+        //
+        //The New button
+        //
+        private void bttNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+        //
+        //The Save button
+        //
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, txtMessage.Text);
+
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+        //
+        //The Read button
+        //
+        private void bttRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if(index > -1)
+            {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                txtMessage.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+
+        }
+        //
+        //The Delete Button
+        //
+        private void bttDel_Click(object sender, EventArgs e) 
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+        }
+    }
+}
